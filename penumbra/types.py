58410becd2e41b2a2a8f8f6bbb20beec532ae69e
Jon McKenzie <jcmcken@gmail.com>
2013-05-27 06:10:50 -0400

    Make datetime converter more robust

--- penumbra/types.py
+++ penumbra/types.py
@@ -2,11 +2,14 @@ import datetime
 from formencode.validators import Int, String, StringBool, Number
 
 def validate_datetime(x):
-    try:
-        result = datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
-    except (ValueError, TypeError), e:
-        raise ValueError('datetime must conform to ISO 8601, i.e. "YYYY-MM-DD HH:MM:SS.mmmmmm"')
-    return result
+    if isinstance(x, datetime.datetime):
+        result = x
+    else:
+        try:
+            result = datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
+        except (ValueError, TypeError), e:
+            raise ValueError('datetime must conform to ISO 8601, i.e. "YYYY-MM-DD HH:MM:SS.mmmmmm"')
+    return str(result) # datetime objects are not JSON-serializable
 
 # Take arbitrary input, validate, and return an object
 #
