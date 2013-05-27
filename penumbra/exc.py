e3c6ed7c66eaeb6735a3ffd2e30b814879b50f4d
Jon McKenzie <jcmcken@gmail.com>
2013-05-27 07:46:07 -0400

    Wrap formencode validation exceptions to equalize with custom exceptions

---
+++ penumbra/exc.py
@@ -0,0 +1,1 @@
+class InvalidDataType(ValueError): pass
--- penumbra/types.py
+++ penumbra/types.py
@@ -1,5 +1,7 @@
 import datetime
 from formencode.validators import Int, String, StringBool, Number
+from formencode.api import Invalid
+from penumbra.exc import InvalidDataType
 
 def validate_datetime(x):
     if isinstance(x, datetime.datetime):
@@ -8,16 +10,24 @@ def validate_datetime(x):
         try:
             result = datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
         except (ValueError, TypeError), e:
-            raise ValueError('datetime must conform to ISO 8601, i.e. "YYYY-MM-DD HH:MM:SS.mmmmmm"')
+            raise InvalidDataType('datetime must conform to ISO 8601, i.e. "YYYY-MM-DD HH:MM:SS.mmmmmm"')
     return str(result) # datetime objects are not JSON-serializable
+
+def _wrap_formencode(func):
+    def wrapped(x):
+        try:
+            func(x)
+        except Invalid, e:
+            raise InvalidDataType(e.args[0])
+    return wrapped
 
 # Take arbitrary input, validate, and return an object
 #
 # To convert back, run ``str`` against the object.
 TYPES = {
-  'float': Number.to_python,
-  'integer': Int.to_python,
-  'string': String.to_python,
-  'boolean': StringBool.to_python,
+  'float': _wrap_formencode(Number.to_python),
+  'integer': _wrap_formencode(Int.to_python),
+  'string': _wrap_formencode(String.to_python),
+  'boolean': _wrap_formencode(StringBool.to_python),
   'datetime': validate_datetime,
 }
