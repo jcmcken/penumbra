d3d9fa718f152fe8235d0da04ac06b3f8ad3e4f1
Jon McKenzie <jcmcken@gmail.com>
2013-05-27 07:12:51 -0400

    Split out key into its own resource, so end user can only deal with pre-defined keys

--- penumbra/models.py
+++ penumbra/models.py
@@ -2,6 +2,7 @@ from penumbra import db
 from penumbra.types import TYPES
 import datetime
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm.exc import NoResultFound
 
 _now = datetime.datetime.now
 
@@ -37,33 +38,45 @@ class Host(BaseMixin, TimestampMixin, db.Model):
     def __repr__(self):
         return "<Host(name=%s, ip=%s)>" % (self.name, self.ip)
 
+class Key(BaseMixin, TimestampMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(length=255), nullable=False, unique=True)
+    # IPv4 addresses are a maximum of 15 chars
+    description = db.Column(db.Text, nullable=False)
+    type = db.Column(db.Enum(*TYPES.keys()), nullable=False)
+    data = db.relationship('Datum', backref='key', lazy='dynamic')
+
+    def __repr__(self):
+        return "<Key(name=%s, type=%s)>" % (self.name, self.type)
+
 class Datum(BaseMixin, TimestampMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    key = db.Column(db.String(length=255), nullable=False)
     # IPv4 addresses are a maximum of 15 chars
     _value = db.Column(db.String(length=15), nullable=False)
-    type = db.Column(db.Enum(*TYPES.keys()), nullable=False)
     host_id = db.Column(db.Integer, db.ForeignKey(Host.id), nullable=False)
+    key_id = db.Column(db.Integer, db.ForeignKey(Key.id), nullable=False)
 
     __table_args__ = (
         # Each host can only have a single key with a certain name
-        db.UniqueConstraint('host_id', 'key', name='_host_key_uc'),
+        db.UniqueConstraint('host_id', 'key_id', name='_host_key_uc'),
     )
 
-    def __init__(self, key, value, type):
-        self.key = key
-        self.type = type
+    def __init__(self, key, value):
+        try:
+            self.key = Key.query.filter_by(name=key).one()
+        except NoResultFound:
+            raise NoResultFound('invalid key "%s"' % key)
         self.value = value
 
     def __repr__(self):
-        return "<Datum(key=%s, value=%s, type=%s)>" % (self.key, self.value, self.type)
+        return "<Datum(key=%s, value=%s, type=%s)>" % (self.key.name, self.value, self.key.type)
 
     @hybrid_property
     def value(self):
-        converter = TYPES.get(self.type)
+        converter = TYPES.get(self.key.type)
         return converter(self._value)
 
     @value.setter
     def value(self, value):
-        converter = TYPES.get(self.type)
+        converter = TYPES.get(self.key.type)
         self._value = str(converter(value))
